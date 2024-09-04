f2b4620d8e25f94a9b2717ae920c846523a55dd9
Stefan <stefan.gore@studenti.unitn.it>
2024-09-04 19:37:51 +0200

    another one

--- PreTopology.py
+++ PreTopology.py
@@ -5,6 +5,8 @@ from mininet.node import OVSKernelSwitch, UserSwitch
 from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 from mininet.link import TCLink, Intf
+from mininet.node import OVSSwitch
+from mininet.log import info, output, warn, setLogLevel
 from _thread import start_new_thread
 import os, stat
 import json
@@ -27,6 +29,80 @@ print(sys.path.__str__())
 ###################################################################
 ############### Scenario - 6 Hosts    #############################
 ###################################################################
+
+class MobilitySwitch( OVSSwitch ):
+    "Switch that can reattach and rename interfaces"
+
+    def delIntf( self, intf ):
+        "Remove (and detach) an interface"
+        port = self.ports[ intf ]
+        del self.ports[ intf ]
+        del self.intfs[ port ]
+        del self.nameToIntf[ intf.name ]
+
+    # pylint: disable=arguments-differ
+    def addIntf( self, intf, rename=False, **kwargs ):
+        "Add (and reparent) an interface"
+        OVSSwitch.addIntf( self, intf, **kwargs )
+        intf.node = self
+        if rename:
+            self.renameIntf( intf )
+
+    def attach( self, intf ):
+        "Attach an interface and set its port"
+        port = self.ports[ intf ]
+        if port:
+            if self.isOldOVS():
+                self.cmd( 'ovs-vsctl add-port', self, intf )
+            else:
+                self.cmd( 'ovs-vsctl add-port', self, intf,
+                          '-- set Interface', intf,
+                          'ofport_request=%s' % port )
+            self.validatePort( intf )
+
+    def validatePort( self, intf ):
+        "Validate intf's OF port number"
+        ofport = int( self.cmd( 'ovs-vsctl get Interface', intf,
+                                'ofport' ) )
+        if ofport != self.ports[ intf ]:
+            warn( 'WARNING: ofport for', intf, 'is actually', ofport,
+                  '\n' )
+
+    def renameIntf( self, intf, newname='' ):
+        "Rename an interface (to its canonical name)"
+        intf.ifconfig( 'down' )
+        if not newname:
+            newname = '%s-eth%d' % ( self.name, self.ports[ intf ] )
+        intf.cmd( 'ip link set', intf, 'name', newname )
+        del self.nameToIntf[ intf.name ]
+        intf.name = newname
+        self.nameToIntf[ intf.name ] = intf
+        intf.ifconfig( 'up' )
+
+    def moveIntf( self, intf, switch, port=None, rename=True ):
+        "Move one of our interfaces to another switch"
+        self.detach( intf )
+        self.delIntf( intf )
+        switch.addIntf( intf, port=port, rename=rename )
+        switch.attach( intf )
+
+def printConnections( switches ):
+    "Compactly print connected nodes to each switch"
+    for sw in switches:
+        output( '%s: ' % sw )
+        for intf in sw.intfList():
+            link = intf.link
+            if link:
+                intf1, intf2 = link.intf1, link.intf2
+                remote = intf1 if intf1.node != sw else intf2
+                output( '%s(%s) ' % ( remote.node, sw.ports[ intf ] ) )
+        output( '\n' )
+
+def moveHost( host, oldSwitch, newSwitch, newPort=None ):
+    "Move a host from old switch to new switch"
+    hintf, sintf = host.connectionsTo( oldSwitch )[ 0 ]
+    oldSwitch.moveIntf( sintf, newSwitch, port=newPort )
+    return hintf, sintf
 
 
 def startIperf(host1, host2, bw, port, timeTotal):
@@ -207,7 +283,7 @@ def four_switches_network():
                     burst=1000000)
     net.addLink(s4, s3, delay='14ms', use_tbf=True, bw=4, max_queue_size=queue_lenght, latency_ms=10000000,
                     burst=1000000)
-    """
+    
     #MORE ADVANCE NEEDING
     net.addLink(h1, s1)
     net.addLink(h2, s1)
@@ -228,7 +304,7 @@ def four_switches_network():
     net.addLink(h4, s3, delay='14ms', use_tbf=True, bw=4, max_queue_size=1000, burst=1000000)
     net.addLink(h5, s3, delay='14ms', use_tbf=True, bw=4, max_queue_size=1000, burst=1000000)
     net.addLink(h6, s3, delay='14ms', use_tbf=True, bw=4, max_queue_size=1000, burst=1000000)
-
+    """
     # Additional link from h1 to s2 for migration
     #net.addLink(h1, s2, delay='14ms', use_tbf=True, bw=4, max_queue_size=1000, burst=1000000)
 
@@ -257,16 +333,12 @@ def four_switches_network():
     #printing topology AFTER changing
     print_topology(net)
     time.sleep(5)
-    terminate_iperf_on_host(h1)
-
-    
+        
     #MIGRATION FUNCTIONS, CHOOSE ONE
-    #migrate_host_2(net, 's1', 's2', 'h1')
-    migrate_host(net, 'h1', 's1', 's2')
-    #link_down(net, 's1', 'h1')
-    #link_up(net, 's2', 'h1')
-    print("starting again iperf h1 h4")
-    start_new_thread(startIperf, (h1, h4, 2.75, 5001, timeTotal))
+    
+    h1, old = net.get('h1', 's1')
+    new = net['s2']
+    hintf, sintf = moveHost(h1, old, new )
 
     time.sleep(5)
     print_topology(net)
